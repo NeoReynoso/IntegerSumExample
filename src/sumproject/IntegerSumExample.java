2bca40c1e3f58f9989d3074fddcf07bdc16b38f5
Ivan Reynoso <ireynoso760@gmail.com>
2024-11-08 14:02:12 -0800

    Updated code

--- src/sumproject/IntegerSumExample.java
+++ src/sumproject/IntegerSumExample.java
@@ -4,8 +4,10 @@ public class IntegerSumExample {
 	public static void main(String[] args) {          
 	      int a = 65;  
 	        int b = 35;  
-	        // It will return the sum of a and b.  
+	        // It will return the sum of a and b. Add  
 	        System.out.println("The sum of a and b is = " + Integer.sum(a, b));  
-	    }  
+	    
+	
+	}  
 
 }
